4b179cd2c9f2fcd5b4e227a9b9ebea1bbd9e02fd
gludovatza <attila@gludovatz.hu>
2023-05-18 20:40:28 +0200

    basic example

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import Basic from "./components/Basic.vue";
+
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
+
+const app = createApp(Basic);
+app.component('EasyDataTable', Vue3EasyDataTable);
+app.mount('#app');
